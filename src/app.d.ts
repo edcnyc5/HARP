6f5502f95ca4c26bc8d74361eb01c4bc64b82437
edc <103957235+mp0228@users.noreply.github.com>
2023-01-10 01:51:12 -0500

    added cf pages support

---
+++ src/app.d.ts
@@ -0,0 +1,14 @@
+declare namespace App {    
+    interface Locals {}
+
+    interface Platform {
+            env: {
+                COUNTER: DurableObjectNamespace;
+                };
+            context: {
+                waitUntil(promise: Promise<any>): void;
+                };
+            caches: CacheStorage & { default: Cache }
+            }
+    interface Session {}
+    interface Stuff {}}
