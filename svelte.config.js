af6843aa496c705f9fbf24927f6d09cad750e497
edc <miguelperez0721@gmail.com>
2023-01-10 03:14:29 -0500

    test commit7

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,10 @@
-import adapter from '@sveltejs/adapter-auto';
-// import adapter from '@sveltejs/adapter-cloudflare';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: cloudflare()
 	}
 };
 
