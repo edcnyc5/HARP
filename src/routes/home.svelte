b588239e6b31c096a86b673dfffa3506a23da06c
edc <miguelperez0721@gmail.com>
2023-01-10 04:23:13 -0500

    added .js to api routes

--- src/routes/home.svelte
+++ src/routes/home.svelte
@@ -134,7 +134,7 @@ async function testAPI(){
     if ($repoSongListRaw.length != 0){
         return;
     } else {
-        const response = await fetch('$lib/routes/api/spaces');
+        const response = await fetch('/api/spaces.js');
         $spacesList = await response.json();
         console.log('contents: ', $spacesList);
         $repoSongListRaw = $spacesList.Contents;
